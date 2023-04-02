ab48e564d65669c64a1162855009ec62d48523e7
Yechiel Babani <51932344+yechielb2000@users.noreply.github.com>
2023-04-02 18:00:32 +0000

    refactoring air_mouse

---
+++ air_mouse.py
@@ -0,0 +1,52 @@
+import socket
+import time
+import bs4
+
+from pynput.mouse import Button, Controller
+
+PORT = 5555
+IP = '192.168.1.17'
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((IP, PORT))
+mouse = Controller()
+
+
+def sensor_data() -> None:
+    oldz = 9
+    sauce = bs4.BeautifulSoup(data, 'lxml')
+    accelerometers = {
+        'accelerometer1': None,
+        'accelerometer2': None,
+        'accelerometer3': None
+    }
+
+    for accelerometer in accelerometers.keys():
+        accelerometers[accelerometer] = int(
+            float(sauce.find(accelerometer).text))
+        print(f'{accelerometer}: {str(accelerometers[accelerometer])}')
+
+    single_click = sauce.find('lightintensity')
+    mouse.move(- int(accelerometers['accelerometer1']) * 6,
+               accelerometers['accelerometer2'] * 6)
+
+    if int(float(single_click.text)) <= 10:
+        mouse.click(Button.left)
+        mouse.release(Button.left)
+        print("Left Click")
+
+    if oldz > accelerometers['accelerometer3']:
+        if oldz - accelerometers['accelerometer3'] > 2:
+            mouse.click(Button.right)
+            mouse.release(Button.right)
+            print("Right Click")
+    else:
+        oldz = accelerometers['accelerometer3']
+
+
+while True:
+    rawdata = sock.recvfrom(PORT)
+    data = str(rawdata)
+    sensor_data()
+    time.sleep(0)
